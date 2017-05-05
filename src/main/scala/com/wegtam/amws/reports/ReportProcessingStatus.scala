0f06902c380ed9113021e019a0702809137db9be
Jens Grassel <jens@wegtam.com>
2017-05-05 10:35:28 +0200

    Add report processing status type.

---
+++ src/main/scala/com/wegtam/amws/reports/ReportProcessingStatus.scala
@@ -0,0 +1,57 @@
+package com.wegtam.amws.reports
+
+import com.wegtam.amws.common.Request.ParameterValue
+
+/**
+  * A report has a processing status.
+  */
+sealed trait ReportProcessingStatus {
+
+  /**
+    * The parameter value of the processing status that can be used
+    * in a query and is also the value returned in responses.
+    *
+    * @return A string representation of the processing status.
+    */
+  def toParameterValue: ParameterValue
+
+}
+
+object ReportProcessingStatus {
+
+  /**
+    * The report has been submitted but no further processing has been done.
+    */
+  case object Submitted extends ReportProcessingStatus {
+    override def toParameterValue: ParameterValue = "_SUBMITTED_"
+  }
+
+  /**
+    * The report is currently being processed.
+    */
+  case object InProgress extends ReportProcessingStatus {
+    override def toParameterValue: ParameterValue = "_IN_PROGRESS_"
+  }
+
+  /**
+    * The processing of the report has been cancelled.
+    */
+  case object Cancelled extends ReportProcessingStatus {
+    override def toParameterValue: ParameterValue = "_CANCELLED_"
+  }
+
+  /**
+    * The processing is done and the report is ready to be fetched.
+    */
+  case object Done extends ReportProcessingStatus {
+    override def toParameterValue: ParameterValue = "_DONE_"
+  }
+
+  /**
+    * The processing is done but the report contains no data.
+    */
+  case object DoneNoData extends ReportProcessingStatus {
+    override def toParameterValue: ParameterValue = "_DONE_NO_DATA_"
+  }
+
+}
